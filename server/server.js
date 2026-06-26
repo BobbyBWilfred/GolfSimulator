fb1d7ce70e5748d9c5aba8cddc5cc4147bc898d9
Bobby B Wilfred <145100872+BobbyBWilfred@users.noreply.github.com>
2026-06-26 21:10:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 
-
+const path = require("path");
+app.use(express.static(path.join(__dirname, "..")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
